b177c46291dc5bc047c5f827f235648e1bb65dbe
tankTopTaro <balmoresk3v1n@gmail.com>
2025-04-04 17:10:08 +0800

    updated GameSession to insert data to game_session, team_game_session, and player_game_session

--- models/gameSession.js
+++ models/gameSession.js
@@ -25,7 +25,7 @@ const GameSession = {
             const [gameResult] = await pool.query(
                `INSERT INTO game_session 
                (date_add, room_type, game_rule, game_level, duration_s_theory, duration_s_actual, game_log, log, is_collaborative, parent_gs_id, facility_id) 
-               VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+               VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
                [
                date_add,
                room_type,
